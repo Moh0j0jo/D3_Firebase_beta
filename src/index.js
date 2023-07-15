0468f07adba58d6b6f6fc9fa2c5984340172c3cd
Peter Pinnel <peter.pinnel@gmail.com>
2023-07-15 15:15:45 +0200

    working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { doc, onSnapshot } from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -19,11 +20,11 @@ const db = getFirestore(app);
 const colRef = collection(db, 'dishes')
 
 
-  // select the svg container first
+// select the svg container first
 const svg = d3.select('.canvas')
-.append('svg')
-  .attr('width', 600)
-  .attr('height', 600);
+  .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
 
 // create margins & dimensions
 const margin = {top: 20, right: 20, bottom: 100, left: 100};
@@ -31,77 +32,118 @@ const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg.append('g')
-.attr('width', graphWidth)
-.attr('height', graphHeight)
-.attr('transform', `translate(${margin.left}, ${margin.top})`);
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 // create axes groups
 const xAxisGroup = graph.append('g')
-.attr('transform', `translate(0, ${graphHeight})`)
+  .attr('transform', `translate(0, ${graphHeight})`)
 
 xAxisGroup.selectAll('text')
-.attr('fill', 'orange')
-.attr('transform', 'rotate(-40)')
-.attr('text-anchor', 'end');
+  .attr('fill', 'orange')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end');
 
 const yAxisGroup = graph.append('g');
 
 const y = d3.scaleLinear()
-  .range([graphHeight, 0]);
+    .range([graphHeight, 0]);
 
 const x = d3.scaleBand()
-.range([0, graphWidth])
-.paddingInner(0.2)
-.paddingOuter(0.2);
+  .range([0, graphWidth])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
 
 // create & call axes
 const xAxis = d3.axisBottom(x);
 const yAxis = d3.axisLeft(y)
-.ticks(3)
-.tickFormat(d => d + ' orders');
+  .ticks(3)
+  .tickFormat(d => d + ' orders');
 
 // the update function
 const update = (data) => {
 
-// join the data to circs
-const rects = graph.selectAll('rect')
-  .data(data);
+  // join the data to circs
+  const rects = graph.selectAll('rect')
+    .data(data);
 
-// update the domains
-y.domain([0, d3.max(data, d => d.orders)]);
-x.domain(data.map(item => item.name));
+  //remove exit selection
+  //rects.exit().remove();
 
-// add attrs to rects already in the DOM
-rects.attr('width', x.bandwidth)
-  .attr("height", d => graphHeight - y(d.orders))
-  .attr('fill', 'orange')
-  .attr('x', d => x(d.name))
-  .attr('y', d => y(d.orders));
+  // update the domains
+  y.domain([0, d3.max(data, d => d.orders)]);
+  x.domain(data.map(item => item.name));
 
-// append the enter selection to the DOM
-rects.enter()
-  .append('rect')
-    .attr('width', x.bandwidth)
+  // add attrs to rects already in the DOM
+  rects.attr('width', x.bandwidth)
     .attr("height", d => graphHeight - y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d) => x(d.name))
+    .attr('x', d => x(d.name))
     .attr('y', d => y(d.orders));
 
-xAxisGroup.call(xAxis);
-yAxisGroup.call(yAxis);
+  // append the enter selection to the DOM
+  rects.enter()
+    .append('rect')
+      .attr('width', x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', (d) => x(d.name))
+      .attr('y', d => y(d.orders));
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
 
 };
 
+// getDocs(colRef)
+//   .then(res => {
 
-getDocs(colRef)
-  .then(res => {
-
-  const data = [];
+//   const data = [];
   
-  res.docs.forEach(doc => {
-    data.push(doc.data());
-  });
+//   res.docs.forEach(doc => {
+//     data.push(doc.data());
+//   });
 
-  update(data);
+//   update(data);
 
-});
+//   d3.interval(()=> {
+//     data[0].orders +=50;
+    
+//     // update(data);
+//   }, 1000)
+
+// });
+
+
+let data = []
+
+const unsub = onSnapshot(colRef, (snapshot)  => {
+ let test = []
+ 
+ 
+ snapshot.forEach( doc => {
+  console.log('Document ID:', doc.id);
+  console.log('Document Data:', doc.data());
+   
+    // const doc = {...doc.data(), id: doc.id}
+    
+    // switch (change.type) {
+    //   case 'added':
+    //     data.push(doc);
+    //     break;
+    //   case 'modified':
+    //     const index = data.findIndex(item => { item.id == doc.id})
+    //     data[index] = doc;
+    //     break;
+    //   case 'removed':
+    //     data = data.filter(item => item.id !== doc.id);
+    //     break;
+    //   default:
+    //     break;
+    // }
+
+  })
+ 
+  // update(data)
+})
